b33a441058110d04372cb6c8a95f111e66b13390
SANTHOSH KUMAR D <santhoshkdhana@gmail.com>
2020-12-01 21:28:55 +0530

    Create two_sum.py

---
+++ two_sum.py
@@ -0,0 +1,20 @@
+def twoSum(nums, target):
+    
+    nums_len = len(nums)
+    #main loop
+    for item in range(nums_len):
+        #inner loop
+        for item2 in range(item+1, nums_len):
+
+            if nums[item] + nums[item2] == target :
+                
+                return [item,item2]
+
+'''
+Input 
+[1,20,15,20]
+40
+
+Output
+[1,3]
+'''
